701bff670ef21d8739a09752514bf1f93590996c
harshaV87 <65055972+harshaV87@users.noreply.github.com>
2023-12-05 22:03:12 -0500

    Update VideoIDExtraction.swift

--- OpinionAnalysis/Algos/VideoIDExtraction.swift
+++ OpinionAnalysis/Algos/VideoIDExtraction.swift
@@ -14,36 +14,24 @@ class extractIDFromURL {
     
     func getIDFromURL(from urlString: String) -> String {
         // an example here is https://www.youtube.com/watch?v=0MTjHRW5WtU&ab_channel=VivekRamaswamy
-        
-        // now, to extract the youtube, we nee do get the array here
-        
+        // Extract chars as array
         let urlArray = Array(urlString)
-        
-        // lets iterate through it now
-        
+        //iterate through it now
         var vArrays = [Int]()
-        
+        // Loop through
         for i in 0..<urlArray.count {
-            // now the condition goes that we have to find the subArray where we need to get the v=
+            // condition for subArray to get the v=
             if urlArray[i] == "v" {
-                // lets make an array and add it to the array here
-                // lets write a check to make sure the program does not crash
-                
+        // redundancy for no crash 
                 if i < urlArray.count - 1 {
                     vArrays.append(i)
-                }
-               
+                } 
             }
         }
-        
-        // now that we have the list of places we have the v, we just need to check iof there is an == after that or ? before that
-        
-        // now lets iterate through again
-        
+        //  v places check for an == after that or ? before that
+        // iterate through again
         let isolatedV = vArrays.filter({urlArray[$0 + 1] == "="})
-        
-       
-        
+        // sub arrays 
         if isolatedV.count == 1 {
             // we expect only 1 isolatedvs here
             var stringOutput = urlArray[(isolatedV[0] + 2)...(isolatedV[0] + 12)]
